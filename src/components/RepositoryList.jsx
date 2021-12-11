c84a9b8b290e1859d786cba2c554f75d49d5a4c6
ESTEVAO <estevao.serpa1234@gmail.com>
2021-12-11 20:20:44 -0300

    rocket modulo1

---
+++ src/components/RepositoryList.jsx
@@ -0,0 +1,34 @@
+export default function RepositoryList(){
+    return(
+        <section className="repository-list">
+            <h1>Lista de repositorios</h1>
+
+            <ul>
+                <li>
+                    <strong>unform</strong>
+                    <p>forms in React</p>
+
+                    <a href="">Acessar repositorio</a>
+                </li>
+                <li>
+                    <strong>unform</strong>
+                    <p>forms in React</p>
+
+                    <a href="">Acessar repositorio</a>
+                </li>
+                <li>
+                    <strong>unform</strong>
+                    <p>forms in React</p>
+
+                    <a href="">Acessar repositorio</a>
+                </li>
+                <li>
+                    <strong>unform</strong>
+                    <p>forms in React</p>
+
+                    <a href="">Acessar repositorio</a>
+                </li>
+            </ul>
+        </section>
+    )
+}
